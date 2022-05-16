da8fafea7ebf663ebb9f70d9594d58267d67f5bb
Naomi Hauret <hauretnaomi@gmail.com>
2022-05-16 18:35:02 +0200

    add l2 chains

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,9 +1,19 @@
-import { createClient } from '@wagmi/core'
+import { createClient, chain } from '@wagmi/core'
 import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask'
 
+const defaultChains = [
+  chain.mainnet,
+  chain.rinkeby,
+  chain.polygon,
+  chain.polygonMumbai,
+  chain.optimism,
+  chain.arbitrum,
+]
 export const client = createClient({
   autoConnect: true,
-  connectors: [new MetaMaskConnector()],
+  connectors: [new MetaMaskConnector({
+    chains: defaultChains,
+  })],
 })
 
 export default client
